697774d56aa4cc3d24f22425661e656744c021b9
dinger171 <davydsokolik666@gmail.com>
2026-07-27 22:26:27 +0200

    Make API for nades search actually work part 3

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,12 +1,14 @@
 import express from "express";
+import cors from "cors";
 import nadesRouter from "./routes/nades.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/nades", nadesRouter);
 
-app.listen(3000, () => {
-    console.log("Backend running on http://localhost:3000");
+app.listen(3000, "::", () => {
+    console.log("Backend running on http://[::]:3000");
 });
--- Frontend/src/api/nades.js
+++ Frontend/src/api/nades.js
@@ -1,6 +1,6 @@
 export async function getNades() {
     const response = await fetch(
-        "http://2a0d:3344:5230:2c00::115/api/nades"
+        "http://[2a0d:3344:5230:2c00::115]:3000/api/nades"
     );
 
     return await response.json();
